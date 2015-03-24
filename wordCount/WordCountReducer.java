1b7a2f854d9859e9c34f0e29c8b80bdb9ba7c18f
suruSharma <suruchi.sharma7389@gmail.com>
2015-03-24 14:19:03 -0400

    Create WordCountReducer.java

---
+++ wordCount/WordCountReducer.java
@@ -0,0 +1,24 @@
+package wordCount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends
+		Reducer<Text, IntWritable, Text, IntWritable> {
+
+	/* (non-Javadoc)
+	 * @see org.apache.hadoop.mapreduce.Reducer#reduce(KEYIN, java.lang.Iterable, org.apache.hadoop.mapreduce.Reducer.Context)
+	 */
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		int count = 0;
+		for (IntWritable value : values) {
+			count += value.get();
+		}
+		context.write(new Text(key+","), new IntWritable(count));
+	}
+}
