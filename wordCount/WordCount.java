d0a78b77b7dba8578f1417fc6042d1ab6d019d3f
suruSharma <suruchi.sharma7389@gmail.com>
2015-03-24 14:18:43 -0400

    Create WordCount.java

---
+++ wordCount/WordCount.java
@@ -0,0 +1,36 @@
+package wordCount;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		if (args.length != 2) {
+			System.err
+					.println("Input parameters are not sufficient");
+			System.exit(-1);
+		}
+		Job job = new Job();
+		job.setJarByClass(WordCount.class);
+		job.setJobName("Word Count");
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+	}
+
+}
